cc2695f0c9e6c4aa123f8515158f7f7c1ff068d9
kingsley78 <85633140+kingsley78@users.noreply.github.com>
2021-07-08 08:09:17 +0100

    Add files via upload

---
+++ flow7.py
@@ -0,0 +1,13 @@
+#flowchart7
+check1 = input("is it raining outside")
+g = "y"
+b = "n"
+for my_check in check1:
+    while my_check == g:
+        my_check = input("have umbrella? ")
+        if my_check == b:
+            print("wait a while")
+            break
+        elif my_check == g:print("go outside")
+        break
+    else: print("go ourside")
